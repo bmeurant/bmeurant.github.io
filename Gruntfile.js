742fc351e8829081bb515b5d05b671a792304f76
Baptiste Meurant <baptiste.meurant@gmail.com>
2014-10-06 14:27:02 +0200

    mobile compatibility

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,31 +2,6 @@ module.exports = function (grunt) {
 
     grunt.initConfig({
 
-        /*less: {
-         compile: {
-         options: {
-         paths: ['less'],
-         yuicompress: true
-         },
-         files: {
-         'css/cv.css': 'less/cv.less'
-         }
-         }
-         },
-         copy: {
-         bower: {
-         files: [
-         {expand: true, cwd: 'bower_components/jquery/', src: ['jquery.min.*'], dest: 'dist/libs/'},
-         {expand: true, cwd: 'bower_components/handlebars/', src: ['handlebars.min.js'], dest: 'dist/libs/'},
-         {expand: true, cwd: 'bower_components/ember/', src: ['ember.js'], dest: 'dist/libs/'},
-         {expand: true, cwd: 'bower_components/ember-data/', src: ['ember-data.js'], dest: 'dist/libs/'},
-         {expand: true, cwd: 'bower_components/bootstrap/dist/css/', src: ['bootstrap.css'], dest: 'dist/libs/'},
-         {expand: true, cwd: 'bower_components/moment/', src: ['moment.js'], dest: 'dist/libs/'},
-         {expand: true, cwd: 'bower_components/moment/lang', src: ['fr.js'], dest: 'dist/libs/lang'}
-
-         ]
-         }
-         },*/
         sass: {
             options: {
                 includePaths: ['bower_components/foundation/scss']
@@ -48,7 +23,6 @@ module.exports = function (grunt) {
         }
     });
 
-    //grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
